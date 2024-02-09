230362372601d09f97d9c7550a9aaaa81ddf8bc6
aaronjpotter93 <aaronjpotter93@gmail.com>
2024-02-09 16:22:30 -0700

    trying to work in two things. plaid transactions of test data. the other being an attempt to get the first filter working

---
+++ plaidAPI.py
@@ -0,0 +1,38 @@
+import json
+from plaid.api import Client
+from plaid.errors import PlaidError
+
+client = Client(client_id='65c6ab809e7d58001ca92ac8', secret='976dc006855e568188aa73e3d82638', environment='sandbox')
+
+# Create a link token
+try:
+    response = client.LinkToken.create(
+        {
+            'user': {
+                'client_user_id': 'user-id',
+            },
+            'client_name': "Your App Name",
+            'products': ['transactions','income','assets'],
+            'country_codes': ['US'],
+            'language': 'en',
+        }
+    )
+    link_token = response['link_token']
+except PlaidError as e:
+    print(e.code, e.message)
+    exit()
+
+# Use the link token where you need it
+
+# Request transactions
+try:
+    response = client.Transactions.get(link_token, start_date='2022-01-01', end_date='2022-01-31')
+    transactions = response['transactions']
+    
+    # Store transactions as JSON file
+    with open('transactions.json', 'w') as file:
+        json.dump(transactions, file)
+    print("Transactions stored in transactions.json")
+        
+except PlaidError as e:
+    print("Error:", e)
---
+++ sqlReader.py
@@ -0,0 +1,24 @@
+import psycopg2
+from psycopg2 import sql
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    host="192.168.0.9",
+    database="budget_boys",
+    user="python",
+    password="python"
+)
+
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+# Example: Query data
+cursor.execute('SELECT * FROM all_transactions')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+# Function to use the data queired from the database to 
+
+# Close the connection
+conn.close()
